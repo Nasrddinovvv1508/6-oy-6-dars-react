0c1b8b4a436662fc47bd55a8f7f06738a343c2f4
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-06-05 01:20:54 +0500

    BrowserRouter, Routes, Route, Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+// react router dom
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
+// pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <BrowserRouter>
+
+        <nav>
+          <h1>My Articles</h1>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+
+        {/* Pages */}
+        <Routes>
+
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/contact' element={<Contact />}/>
+
+        </Routes>
+
+      </BrowserRouter>
+    </div>
   )
 }
 
---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+function Home() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam doloribus et velit veritatis quasi. Est eveniet mollitia a nihil. Iure et eius ipsum ratione. Molestiae velit repellat quos optio quibusdam delectus veritatis aliquam ipsum quisquam! Dolore pariatur soluta voluptas delectus impedit veritatis, facere amet eligendi, commodi obcaecati, voluptatibus nobis itaque.
+            </p>
+        </div>
+    )
+}
+
+export default Home
