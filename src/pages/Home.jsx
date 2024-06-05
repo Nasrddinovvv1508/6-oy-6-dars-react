2c727986cacf18f2f342d1eb32d64cc0137f028b
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-06-05 14:29:56 +0500

    Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,18 @@ import '../Home.css'
 
 function Home() {
     return (
-        <div className="Home container-speacial">
-            <h1>Find the best <span>talent</span></h1>
+        <div className="Home">
+
+            {/* <img className='group' src="./src/assets/Group8.png" alt="group" /> */}
+
             <div>
-                <img src="./src/assets/line.svg" alt="line" />
-                <p>
-                Finding the right people and building high performing teams can be hard. Most companies aren’t tapping into the abundance of global talent. We’re about to change that.
-                </p>
+                <h1>Find the best <span>talent</span></h1>
+                <div className='div'>
+                    <img src="./src/assets/line.svg" alt="line" />
+                    <p>
+                        Finding the right people and building high performing teams can be hard. Most companies aren’t tapping into the abundance of global talent. We’re about to change that.
+                    </p>
+                </div>
             </div>
         </div>
     )
